69f8fcdea85dcce1201f98b190b8e67b95588d98
Marcel <Marcel@130.229.180.165>
2012-11-09 14:13:22 +0100

    Everything working, except notifycations and callbacks

--- id2212-hw2/src/id2212/hw2/client/mainClient.java
+++ id2212-hw2/src/id2212/hw2/client/mainClient.java
@@ -4,6 +4,7 @@
  */
 package id2212.hw2.client;
 
+import id2212.hw2.client.view.ClientUI;
 import id2212.hw2.item.Item;
 import java.rmi.RemoteException;
 
@@ -17,9 +18,11 @@ public class mainClient {
     
     public static void main(String[] args) throws RemoteException {
         ClientImpl c = new ClientImpl(clientName, "EBAY");
-        c.servObj.registerClient(clientName);
+        ClientUI cf = new ClientUI(c);
+        cf.setVisible(true);
+        /*c.servObj.registerClient(clientName);
         Item itm = new Item("camera",3000);
         c.servObj.sellItem(itm, clientName, c);
-        System.out.println(c.servObj.inspectItem().get(0).name);
+        System.out.println(c.servObj.inspectItem().get(0).name);*/
     }
 }
